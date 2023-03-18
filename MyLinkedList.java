c98c2accd8192e6bf18d7ae75e2ade789129f4d3
Desale1 <dkdesale08@gmail.com>
2023-03-18 22:30:32 +0530

    Use LinkedList to do the Hash Table Operation

---
+++ MyLinkedList.java
@@ -0,0 +1,60 @@
+package com.bridgelabz.map;
+
+import java.util.ArrayList;
+
+
+public class MyLinkedList<K> {
+
+    private INode<K> head;
+    private INode<K> tail;
+
+    public INode<K> getHead() {
+        return head;
+    }
+
+    public void setHead(INode<K> head) {
+        this.head = head;
+    }
+
+    public INode<K> getTail() {
+        return tail;
+    }
+
+    public void append(INode<K> newNode) {
+        if (this.head == null) this.head = newNode;
+
+        if (this.tail == null) this.tail = newNode;
+        else {
+            INode<K> tempNode = this.tail;
+            this.tail = newNode;
+            tempNode.setNext(tail);
+        }
+
+    }
+
+    public INode<K> search(K key) {
+        INode<K> tempNode = head;
+        if (tempNode != null) {
+            while (tempNode.getNext() != null) {
+                if (tempNode.getKey().equals(key)) return tempNode;
+                tempNode = tempNode.getNext();
+            }
+            if (tail.getKey().equals(key)) return tail;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer myMapNodeString = new StringBuffer();
+        INode<K> temp = head;
+        while (temp.getNext() != null) {
+            myMapNodeString.append(temp).append(" -> ");
+            temp = temp.getNext();
+        }
+        myMapNodeString.append(temp);
+        return "LinkedList: {" + myMapNodeString + '}';
+    }
+
+
+}
