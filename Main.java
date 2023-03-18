5f69f9e15d63d0f415f2848aba46614df2c920d5
Desale1 <dkdesale08@gmail.com>
2023-03-18 22:33:51 +0530

    Using main method find frequency of word

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.map;
+
+public class Main {
+    public static void main(String[] args) {
+        MyHashMap<String, Integer> hashMap = new MyHashMap<>();
+        String str = "To be or not to be";
+        String[] strArr = str.toLowerCase().split(" ");
+
+        for (String word : strArr) {
+            Integer value = hashMap.get(word);
+            if (value == null) value = 1;
+            else value += 1;
+            hashMap.add(word, value);
+        }
+        System.out.println(hashMap);
+        System.out.println("frequency of 'to': " + hashMap.get("to"));
+    }
+}
+
